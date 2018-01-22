50b2f233d075ab34fb38e42bccf87fbfbbd81d41
synend16 <synend16@student.westerdals.no>
2018-01-22 22:08:44 +0100

    simple working spring boot application with get and post functionality.

---
+++ src/main/java/com/endre/spring/topic/TopicService.java
@@ -0,0 +1,28 @@
+package com.endre.spring.topic;
+
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class TopicService {
+
+    private List<Topic> topics = Arrays.asList(
+            new Topic("spring", "spring Framework", "Spring Framework description"),
+            new Topic("java", "Core Java", "Core java description"),
+            new Topic("javascript", "JavaScript", "JavaScript description")
+    );
+
+    public List<Topic> getTopics() {
+        return topics;
+    }
+
+    public Topic getTopic(String id){
+        return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+    }
+
+    public void addTopic(Topic topic) {
+        topics.add(topic);
+    }
+}

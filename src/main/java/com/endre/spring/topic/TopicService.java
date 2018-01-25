7bfb78c43b86d1bb2f78b6849a48d9c19bb3bfcd
synend16 <synend16@student.westerdals.no>
2018-01-25 13:36:31 +0100

    Added methods for put and delete

--- src/main/java/com/endre/spring/topic/TopicService.java
+++ src/main/java/com/endre/spring/topic/TopicService.java
@@ -2,17 +2,18 @@ package com.endre.spring.topic;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class TopicService {
 
-    private List<Topic> topics = Arrays.asList(
+    private List<Topic> topics = new ArrayList<>(Arrays.asList(
             new Topic("spring", "spring Framework", "Spring Framework description"),
             new Topic("java", "Core Java", "Core java description"),
             new Topic("javascript", "JavaScript", "JavaScript description")
-    );
+    ));
 
     public List<Topic> getTopics() {
         return topics;
@@ -24,5 +25,19 @@ public class TopicService {
 
     public void addTopic(Topic topic) {
         topics.add(topic);
+    }
+
+    public void updateTopic(String id, Topic topic) {
+        for (int i = 0; i < topics.size(); i++) {
+            Topic t = topics.get(i);
+            if (t.getId().equals(id)){
+                topics.set(i, topic);
+                return;
+            }
+        }
+    }
+
+    public void deleteTopic(String id) {
+        topics.removeIf(t -> t.getId().equals(id));
     }
 }
